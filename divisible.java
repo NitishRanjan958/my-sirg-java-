1e922d73f1b8a4018807f8d38e5f764081c3d15d
NitishRanjan958 <151854731+NitishRanjan958@users.noreply.github.com>
2025-03-27 21:18:48 +0530

    Add files via upload

---
+++ divisible.java
@@ -0,0 +1,10 @@
+/*write a program to check whether a given number is divisiable by 5 or not */
+/public class divisible {
+    public static void main(String[] args) {
+        int x = 44;
+        if (x % 5 == 0)
+            System.out.println("divisible by 5");
+        else
+            System.out.println("not divisible by 5");
+    }
+}
